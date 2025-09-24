7fa93a943decc929df611336864fa60c047f35a0
mefodiydemin@gmail.com <mefodiydemin@gmail.com>
2025-09-24 14:02:23 +0500

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            TaskProcessor processor = new TaskProcessor();
+
+            Console.WriteLine("Выберите задачу:");
+            Console.WriteLine("Задания 1");
+            Console.WriteLine("1 - Проверка числа на положительность");
+            Console.WriteLine("2 - Сумма двух последних цифр числа");
+            Console.WriteLine("3 - Проверка, заглавный ли символ");
+            Console.WriteLine("4 - Проверка деления чисел нацело");
+            Console.WriteLine("5 - Многократный вызов lastNumSum");
+            Console.WriteLine("Задания 2");
+            Console.WriteLine("6 - Безопасное деление");
+            Console.WriteLine("7 - Строка сравнения");
+            Console.WriteLine("8 - Тройная сумма");
+            Console.WriteLine("9 - Возраст");
+            Console.WriteLine("10 - Вывод дней недели");
+            Console.WriteLine("Задания 3");
+            Console.WriteLine("11 - Числа наоборот");
+            Console.WriteLine("12 - Степень числа");
+            Console.WriteLine("13 - Проверка одинаковости цифр числа");
+            Console.WriteLine("14 - Левый треугольник");
+            Console.WriteLine("15 - Угадайка");
+            Console.WriteLine("Задания 4");
+            Console.WriteLine("16 - Поиск последнего вхождения");
+            Console.WriteLine("17 - Добавление в массив");
+            Console.WriteLine("18 - Реверс массива");
+            Console.WriteLine("19 - Объединение массивов");
+            Console.WriteLine("20 - Удалить негатив");
+            Console.Write("Ваш выбор: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Введите число: ");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    bool result1 = processor.IsPositive(number);
+                    if (result1)
+                        Console.WriteLine("true");
+                    else
+                        Console.WriteLine("false");
+                    break;
+
+                case "2":
+                    Console.Write("Введите число: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    if (Math.Abs(num) < 10)
+                        Console.WriteLine("Ошибка: число должно содержать минимум две цифры!");
+                    else
+                        Console.WriteLine("Сумма двух последних цифр: " + processor.SumLastNums(num));
+                    break;
+
+                case "3":
+                    Console.Write("Введите символ: ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    bool result3 = processor.IsUpperCase(ch);
+                    if (result3)
+                        Console.WriteLine("true");
+                    else
+                        Console.WriteLine("false");
+                    break;
+
+                case "4":
+                    Console.Write("Введите первое число: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    bool result4 = processor.IsDivisor(a, b);
+                    if (result4)
+                        Console.WriteLine("true");
+                    else
+                        Console.WriteLine("false");
+                    break;
+
+                case "5":
+                    int result = 0;
+
+                    Console.Write("Введите число 1: ");
+                    int firstNum = Convert.ToInt32(Console.ReadLine());
+                    result = Math.Abs(firstNum) % 10;
+
+                    for (int i = 2; i <= 5; i++)
+                    {
+                        Console.Write("Введите число " + i + ": ");
+                        int currentNum = Convert.ToInt32(Console.ReadLine());
+                        int previousResult = result;
+                        result = processor.LastNumSum(result, currentNum);
+                        Console.WriteLine(previousResult + "+" + currentNum + " это " + result);
+                    }
+
+                    Console.WriteLine("Итоговый результат: " + result);
+                    break;
+
+                case "6":
+                    Console.Write("Введите делимое (x): ");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите делитель (y): ");
+                    int y = Convert.ToInt32(Console.ReadLine());
+
+                    double divisionResult = processor.SafeDiv(x, y);
+                    Console.WriteLine("Результат деления: " + divisionResult);
+                    break;
+
+                case "7":
+                    Console.Write("Введите первое число (x): ");
+                    int xVal = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите второе число (y): ");
+                    int yVal = Convert.ToInt32(Console.ReadLine());
+
+                    string comparisonResult = processor.MakeDecision(xVal, yVal);
+                    Console.WriteLine("Результат сравнения: " + comparisonResult);
+                    return;
+
+                case "8":
+                    Console.Write("Введите первое число: ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите третье число: ");
+                    int num3 = Convert.ToInt32(Console.ReadLine());
+
+                    bool sum3Result = processor.Sum3(num1, num2, num3);
+                    if (sum3Result)
+                        Console.WriteLine("true");
+                    else
+                        Console.WriteLine("false");
+                    break;
+
+                case "9":
+                    Console.Write("Введите возраст: ");
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    string ageResult = processor.Age(age);
+                    Console.WriteLine(ageResult);
+                    break;
+
+                case "10":
+                    Console.Write("Введите день недели: ");
+                    string day = Console.ReadLine();
+                    processor.PrintDays(day);
+                    break;
+
+                case "11":
+                    Console.Write("Введите число: ");
+                    int inputNum = Convert.ToInt32(Console.ReadLine());
+                    string reversedList = processor.ReverseListNums(inputNum);
+                    Console.WriteLine(reversedList);
+                    break;
+
+                case "12":
+                    Console.Write("Введите основание степени (x): ");
+                    int baseNum = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите показатель степени (y): ");
+                    int exponent = Convert.ToInt32(Console.ReadLine());
+
+                    int powerResult = processor.Pow(baseNum, exponent);
+                    Console.WriteLine("Результат: " + powerResult);
+                    break;
+
+                case "13":
+                    Console.Write("Введите число: ");
+                    int checkNum = Convert.ToInt32(Console.ReadLine());
+                    bool equalResult = processor.EqualNum(checkNum);
+                    if (equalResult)
+                        Console.WriteLine("true");
+                    else
+                        Console.WriteLine("false");
+                    break;
+
+                case "14":
+                    Console.Write("Введите высоту треугольника: ");
+                    int height = Convert.ToInt32(Console.ReadLine());
+                    processor.LeftTriangle(height);
+                    break;
+
+                case "15":
+                    processor.GuessGame();
+                    break;
+
+                case "16":
+                    Console.WriteLine("Введите элементы массива через пробел:");
+                    string[] inputArray = Console.ReadLine().Split(' ');
+                    int[] arr = new int[inputArray.Length];
+
+                    for (int i = 0; i < inputArray.Length; i++)
+                    {
+                        arr[i] = Convert.ToInt32(inputArray[i]);
+                    }
+
+                    Console.Write("Введите число для поиска: ");
+                    int searchNum = Convert.ToInt32(Console.ReadLine());
+
+                    int lastIndex = processor.FindLast(arr, searchNum);
+                    Console.WriteLine("Индекс последнего вхождения: " + lastIndex);
+                    break;
+
+                case "17":
+                    Console.WriteLine("Введите элементы массива через пробел:");
+                    string[] inputArr = Console.ReadLine().Split(' ');
+                    int[] originalArray = new int[inputArr.Length];
+
+                    for (int i = 0; i < inputArr.Length; i++)
+                    {
+                        originalArray[i] = Convert.ToInt32(inputArr[i]);
+                    }
+
+                    Console.Write("Введите число для добавления: ");
+                    int newValue = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Введите позицию для вставки: ");
+                    int position = Convert.ToInt32(Console.ReadLine());
+
+                    int[] newArray = processor.Add(originalArray, newValue, position);
+
+                    Console.WriteLine("Исходный массив: [" + string.Join(", ", originalArray) + "]");
+                    Console.WriteLine("Новый массив: [" + string.Join(", ", newArray) + "]");
+                    break;
+
+                case "18":
+                    Console.WriteLine("Введите элементы массива через пробел:");
+                    string[] inputReverse = Console.ReadLine().Split(' ');
+                    int[] reverseArray = new int[inputReverse.Length];
+
+                    for (int i = 0; i < inputReverse.Length; i++)
+                    {
+                        reverseArray[i] = Convert.ToInt32(inputReverse[i]);
+                    }
+
+                    Console.WriteLine("Исходный массив: [" + string.Join(", ", reverseArray) + "]");
+                    processor.Reverse(reverseArray);
+                    Console.WriteLine("Реверсированный массив: [" + string.Join(", ", reverseArray) + "]");
+                    break;
+
+                case "19":
+                    Console.WriteLine("Введите элементы первого массива через пробел:");
+                    string[] inputArr1 = Console.ReadLine().Split(' ');
+                    int[] array1 = new int[inputArr1.Length];
+
+                    for (int i = 0; i < inputArr1.Length; i++)
+                    {
+                        array1[i] = Convert.ToInt32(inputArr1[i]);
+                    }
+
+                    Console.WriteLine("Введите элементы второго массива через пробел:");
+                    string[] inputArr2 = Console.ReadLine().Split(' ');
+                    int[] array2 = new int[inputArr2.Length];
+
+                    for (int i = 0; i < inputArr2.Length; i++)
+                    {
+                        array2[i] = Convert.ToInt32(inputArr2[i]);
+                    }
+
+                    int[] concatenatedArray = processor.Concat(array1, array2);
+
+                    Console.WriteLine("Первый массив: [" + string.Join(", ", array1) + "]");
+                    Console.WriteLine("Второй массив: [" + string.Join(", ", array2) + "]");
+                    Console.WriteLine("Объединенный массив: [" + string.Join(", ", concatenatedArray) + "]");
+                    break;
+
+                case "20":
+                    Console.WriteLine("Введите элементы массива через пробел:");
+                    string[] inputNegative = Console.ReadLine().Split(' ');
+                    int[] negativeArray = new int[inputNegative.Length];
+
+                    for (int i = 0; i < inputNegative.Length; i++)
+                    {
+                        negativeArray[i] = Convert.ToInt32(inputNegative[i]);
+                    }
+
+                    int[] positiveArray = processor.DeleteNegative(negativeArray);
+
+                    Console.WriteLine("Исходный массив: [" + string.Join(", ", negativeArray) + "]");
+                    Console.WriteLine("Массив без отрицательных элементов: [" + string.Join(", ", positiveArray) + "]");
+                    break;
+
+                default:
+                    Console.WriteLine("Некорректный ввод!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Tasks.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+
+    public class TaskProcessor
+    {
+        // Проверка положительности числа
+        public bool IsPositive(int x)
+        {
+            if (x > 0)
+                return true;
+            else
+                return false;
+        }
+
+        // Сумма двух последних цифр числа
+        public int SumLastNums(int x)
+        {
+            int num = Math.Abs(x);
+            int lastDigit = num % 10;
+            int secondLastDigit = (num / 10) % 10;
+            return lastDigit + secondLastDigit;
+        }
+
+        // Проверка заглавной буквы
+        public bool IsUpperCase(char x)
+        {
+            if (char.IsUpper(x))
+                return true;
+            else
+                return false;
+        }
+
+        // Проверка, делит ли одно число другое нацело
+        public bool IsDivisor(int a, int b)
+        {
+            if ((b != 0 && a % b == 0) || (a != 0 && b % a == 0))
+                return true;
+            else
+                return false;
+        }
+
+        // Сумма последних цифр двух чисел
+        public int LastNumSum(int a, int b)
+        {
+            return (Math.Abs(a) % 10) + (Math.Abs(b) % 10);
+        }
+
+        // Безопасное деление с проверкой на ноль
+        public double SafeDiv(int x, int y)
+        {
+            if (y == 0)
+                return 0;
+            else
+                return (double)x / y;
+        }
+
+        // Сравнение двух чисел и формирование строки с результатом
+        public string MakeDecision(int x, int y)
+        {
+            if (x > y)
+                return x + " > " + y;
+            else if (x < y)
+                return x + " < " + y;
+            else
+                return x + " == " + y;
+        }
+
+        // Проверка, равна ли сумма двух чисел третьему
+        public bool Sum3(int x, int y, int z)
+        {
+            if (x + y == z || x + z == y || y + z == x)
+                return true;
+            else
+                return false;
+        }
+
+        // Формирование строки с возрастом и правильным склонением
+        public string Age(int x)
+        {
+            int lastDigit = Math.Abs(x) % 10;
+            int lastTwoDigits = Math.Abs(x) % 100;
+
+            // Исключения для чисел 11-14
+            if (lastTwoDigits >= 11 && lastTwoDigits <= 14)
+                return x + " лет";
+
+            if (lastDigit == 1)
+                return x + " год";
+            else if (lastDigit >= 2 && lastDigit <= 4)
+                return x + " года";
+            else
+                return x + " лет";
+        }
+
+        // Вывод дней недели начиная с заданного
+        public void PrintDays(string x)
+        {
+            string day = x.ToLower();
+
+            switch (day)
+            {
+                case "понедельник":
+                    Console.WriteLine("понедельник");
+                    Console.WriteLine("вторник");
+                    Console.WriteLine("среда");
+                    Console.WriteLine("четверг");
+                    Console.WriteLine("пятница");
+                    Console.WriteLine("суббота");
+                    Console.WriteLine("воскресенье");
+                    break;
+
+                case "вторник":
+                    Console.WriteLine("вторник");
+                    Console.WriteLine("среда");
+                    Console.WriteLine("четверг");
+                    Console.WriteLine("пятница");
+                    Console.WriteLine("суббота");
+                    Console.WriteLine("воскресенье");
+                    break;
+
+                case "среда":
+                    Console.WriteLine("среда");
+                    Console.WriteLine("четверг");
+                    Console.WriteLine("пятница");
+                    Console.WriteLine("суббота");
+                    Console.WriteLine("воскресенье");
+                    break;
+
+                case "четверг":
+                    Console.WriteLine("четверг");
+                    Console.WriteLine("пятница");
+                    Console.WriteLine("суббота");
+                    Console.WriteLine("воскресенье");
+                    break;
+
+                case "пятница":
+                    Console.WriteLine("пятница");
+                    Console.WriteLine("суббота");
+                    Console.WriteLine("воскресенье");
+                    break;
+
+                case "суббота":
+                    Console.WriteLine("суббота");
+                    Console.WriteLine("воскресенье");
+                    break;
+
+                case "воскресенье":
+                    Console.WriteLine("воскресенье");
+                    break;
+
+                default:
+                    Console.WriteLine("это не день недели");
+                    break;
+            }
+        }
+
+        // Формирование строки с числами от x до 0
+        public string ReverseListNums(int x)
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (x >= 0)
+            {
+                for (int i = x; i >= 0; i--)
+                {
+                    result.Append(i);
+                    if (i > 0) result.Append(" ");
+                }
+            }
+            else
+            {
+                for (int i = x; i <= 0; i++)
+                {
+                    result.Append(i);
+                    if (i < 0) result.Append(" ");
+                }
+            }
+
+            return result.ToString();
+        }
+
+        // Возведение числа x в степень y
+        public int Pow(int x, int y)
+        {
+            int result = 1;
+
+            for (int i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+
+            return result;
+        }
+
+        // Проверка, все ли цифры в числе одинаковы
+        public bool EqualNum(int x)
+        {
+            int num = Math.Abs(x);
+
+            // Однозначное число всегда имеет одинаковые цифры
+            if (num < 10)
+                return true;
+
+            int lastDigit = num % 10;
+            num /= 10;
+
+            // Проверяем каждую цифру
+            while (num > 0)
+            {
+                int currentDigit = num % 10;
+                if (currentDigit != lastDigit)
+                    return false;
+                num /= 10;
+            }
+
+            return true;
+        }
+
+        // Отрисовка левого треугольника из звездочек
+        public void LeftTriangle(int x)
+        {
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Игра "Угадай число"
+        public void GuessGame()
+        {
+            Random random = new Random();
+            int secretNumber = random.Next(0, 10);
+            int attempts = 0;
+            int userGuess;
+
+            Console.WriteLine("Угадайте число от 0 до 9:");
+
+            do
+            {
+                Console.Write("Введите число: ");
+                userGuess = Convert.ToInt32(Console.ReadLine());
+                attempts++;
+
+                if (userGuess == secretNumber)
+                {
+                    Console.WriteLine("Вы угадали!");
+                }
+                else
+                {
+                    Console.WriteLine("Вы не угадали, введите число от 0 до 9:");
+                }
+            } while (userGuess != secretNumber);
+
+            Console.WriteLine("Вы отгадали число за " + attempts + " попыток(ки)");
+        }
+
+        // Поиск последнего вхождения числа в массиве
+        public int FindLast(int[] arr, int x)
+        {
+            // Ищем с конца массива
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (arr[i] == x)
+                    return i;
+            }
+            return -1;
+        }
+
+        // Вставка элемента в массив на указанную позицию
+        public int[] Add(int[] arr, int x, int pos)
+        {
+            if (pos < 0 || pos > arr.Length)
+            {
+                Console.WriteLine("Ошибка: позиция выходит за пределы массива!");
+                return arr;
+            }
+
+            int[] newArr = new int[arr.Length + 1];
+
+            // Копируем элементы до позиции вставки
+            for (int i = 0; i < pos; i++)
+            {
+                newArr[i] = arr[i];
+            }
+
+            newArr[pos] = x;
+
+            // Копируем элементы после позиции вставки
+            for (int i = pos; i < arr.Length; i++)
+            {
+                newArr[i + 1] = arr[i];
+            }
+
+            return newArr;
+        }
+
+        // Реверс массива (изменение порядка элементов на противоположный)
+        public void Reverse(int[] arr)
+        {
+            // Меняем местами элементы с начала и конца
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                int temp = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length - 1 - i] = temp;
+            }
+        }
+
+        // Объединение двух массивов
+        public int[] Concat(int[] arr1, int[] arr2)
+        {
+            int[] result = new int[arr1.Length + arr2.Length];
+
+            // Копируем первый массив
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                result[i] = arr1[i];
+            }
+
+            // Копируем второй массив
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                result[arr1.Length + i] = arr2[i];
+            }
+
+            return result;
+        }
+
+        // Удаление отрицательных элементов из массива
+        public int[] DeleteNegative(int[] arr)
+        {
+            // Считаем количество неотрицательных элементов
+            int count = 0;
+            foreach (int num in arr)
+            {
+                if (num >= 0)
+                    count++;
+            }
+
+            int[] result = new int[count];
+            int index = 0;
+
+            // Заполняем новый массив
+            foreach (int num in arr)
+            {
+                if (num >= 0)
+                {
+                    result[index] = num;
+                    index++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
